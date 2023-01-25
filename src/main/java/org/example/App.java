7f05e3038cc95936d3c11dc7a5cc8029129a1389
Fady <fady.fouad.a@gmail.com>
2023-01-25 13:16:53 +0200

    get Number of questions

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,12 +1,9 @@
 package org.example;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
@@ -17,25 +14,32 @@ public class App
 
         // Navigate to Nagwa website and select language
         driver.get("https://nagwa.com");
-        driver.findElement(By.linkText("English")).click();
-        driver.findElement(By.linkText("العربية")).click();
+
+        // Select language
+        driver.findElement(By.xpath("/html/body/header/div[1]/div/div[2]/ul/li[1]/a/span")).click();
+        driver.findElement(By.xpath("/html/body/header/div[1]/div/div[2]/ul/li[1]/ul/li[1]/a")).click();
 
         // Search for lesson
-        driver.findElement(By.cssSelector("#search-icon")).click();
-        driver.findElement(By.cssSelector("#search-input")).sendKeys("addition");
-        driver.findElement(By.cssSelector("#search-button")).click();
+        driver.findElement(By.cssSelector("body > header > div.top-header > div > div.search > button")).click();
+        driver.findElement(By.id("txt_search_query")).sendKeys("addition");
+        driver.findElement(By.xpath("//*[@id=\"btn_global_search\"]/i")).click();
 
         // Click on 2nd result in search results
-        driver.findElement(By.cssSelector("#search-results li:nth-child(2) a")).click();
+        driver.findElement(By.xpath("/html/body/div/div[1]/div/div/main/div[3]/ul/li[2]/div/a")).click();
+
+        //Scroll down till the bottom of the page
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,document.body.scrollHeight)");
 
         // Go to worksheet section and click preview button
-        driver.findElement(By.cssSelector("#worksheet-section button")).click();
+        driver.findElement(By.xpath("//*[@id=\"WorksheetSection\"]/div/div[2]/div/a")).click();
 
         // Count number of questions on worksheet
-        int questionCount = driver.findElements(By.cssSelector("#worksheet-questions li")).size();
+        int questionCount = driver.findElements(By.xpath("/html/body/div/div[1]/div/div/main/div[2]/div")).size();
 
         // Print number of questions
         System.out.println("Number of questions: " + questionCount);
+
 
         // Close browser
         driver.close();
